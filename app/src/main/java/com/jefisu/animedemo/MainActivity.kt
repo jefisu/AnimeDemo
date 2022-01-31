706d021a4dfd5a5f2df76649a18027145ceb9eb2
Jeferson Coutinho <jefissuu@gmail.com>
2022-01-30 21:21:47 -0300

    Implemented SplashScreen

--- app/src/main/java/com/jefisu/animedemo/MainActivity.kt
+++ app/src/main/java/com/jefisu/animedemo/MainActivity.kt
@@ -3,7 +3,10 @@ package com.jefisu.animedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.jefisu.animedemo.ui.theme.presentation.NavGraphs
+import com.jefisu.animedemo.ui.theme.presentation.splash.SplashViewModel
 import com.ramcosta.composedestinations.DestinationsNavHost
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,6 +14,12 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val viewModel by viewModels<SplashViewModel>()
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.isLoading.value
+            }
+        }
         setContent {
             DestinationsNavHost(navGraph = NavGraphs.root)
         }
